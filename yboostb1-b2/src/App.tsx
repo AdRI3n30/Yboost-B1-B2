64cc5ac38710b417ae92ddcd794627ba1253c660
ER1707 <ewan.17.remo@gmail.com>
2024-11-20 16:34:08 +0100

    page Login

--- yboostb1-b2/src/App.tsx
+++ yboostb1-b2/src/App.tsx
@@ -5,7 +5,7 @@ import Contact from './pages/Contact.tsx';
 import List from './pages/List.tsx';
 import Favori from './pages/Favori.tsx';
 import Cocktail from './pages/Cocktail.tsx';
-
+import Login from './pages/Login.tsx';
 
 const App = () => {
   return (
@@ -17,6 +17,7 @@ const App = () => {
         <Route path="/list" element={<List />} />
         <Route path="/favori" element={<Favori />} />
         <Route path="/cocktail" element={<Cocktail />} />
+        <Route path="/formulaire" element={<Login />} />
       </Routes>
     </Router>
   );
