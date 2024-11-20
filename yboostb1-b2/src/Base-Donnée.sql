cc4f4e27639cdbeeded2930ae510b56b8ba2768c
N0wah <noah68mollet@gmail.com>
2024-11-20 14:24:54 +0100

    Update DB

--- yboostb1-b2/src/Base-Donnée.sql
+++ yboostb1-b2/src/Base-Donnée.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 20 nov. 2024 à 09:24
+-- Généré le : mer. 20 nov. 2024 à 13:23
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -29,68 +29,26 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `cocktail`;
 CREATE TABLE IF NOT EXISTS `cocktail` (
-  `Id` varchar(0) DEFAULT NULL,
-  `Name` varchar(0) DEFAULT NULL,
-  `Descri` varchar(0) DEFAULT NULL,
-  `Id_difficulte` varchar(0) DEFAULT NULL,
-  `Image` varchar(0) DEFAULT NULL,
-  `Ingredients` varchar(0) DEFAULT NULL,
-  `Temps` varchar(0) DEFAULT NULL
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(150) NOT NULL,
+  `Descri` text,
+  `Id_difficulte` int DEFAULT NULL,
+  `Image` varchar(255) DEFAULT NULL,
+  `Ingredients` text,
+  `Temps` int DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `Id_difficulte` (`Id_difficulte`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
-
 --
--- Structure de la table `difficulte`
+-- Contraintes pour les tables déchargées
 --
 
-DROP TABLE IF EXISTS `difficulte`;
-CREATE TABLE IF NOT EXISTS `difficulte` (
-  `Id_difficulte` varchar(0) DEFAULT NULL,
-  `Difficulte` varchar(0) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
 --
--- Structure de la table `like`
+-- Contraintes pour la table `cocktail`
 --
-
-DROP TABLE IF EXISTS `like`;
-CREATE TABLE IF NOT EXISTS `like` (
-  `IdLike` varchar(0) DEFAULT NULL,
-  `IdUser` varchar(0) DEFAULT NULL,
-  `IdCocktail` varchar(0) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `sqlite_sequence`
---
-
-DROP TABLE IF EXISTS `sqlite_sequence`;
-CREATE TABLE IF NOT EXISTS `sqlite_sequence` (
-  `name` varchar(0) DEFAULT NULL,
-  `seq` varchar(0) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `utilisateur`
---
-
-DROP TABLE IF EXISTS `utilisateur`;
-CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `Id` varchar(0) DEFAULT NULL,
-  `Pseudo` varchar(0) DEFAULT NULL,
-  `LastName` varchar(0) DEFAULT NULL,
-  `FirstName` varchar(0) DEFAULT NULL,
-  `Email` varchar(0) DEFAULT NULL,
-  `Password` varchar(0) DEFAULT NULL,
-  `Image` varchar(0) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+ALTER TABLE `cocktail`
+  ADD CONSTRAINT `cocktail_ibfk_1` FOREIGN KEY (`Id_difficulte`) REFERENCES `difficulte` (`Id_difficulte`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
