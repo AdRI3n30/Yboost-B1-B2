cb4fdb0e182c987887880b98f15f58f540747b02
Along <144777531+AlongkornFR@users.noreply.github.com>
2024-11-20 12:25:07 +0100

    Composant_searchbar

--- yboostb1-b2/src/Composant/Landing.tsx
+++ yboostb1-b2/src/Composant/Landing.tsx
@@ -3,15 +3,6 @@ import React from 'react';
 const Landing = () => {
     return (
       <><div className="relative w-11/12 max-w-md mt-4">
-        <input
-          type="text"
-          placeholder="Rechercher"
-          className="w-full bg-gray-800 text-white placeholder-gray-500 rounded-full py-2 pl-4 pr-10 focus:outline-none" />
-        <button className="absolute right-3 top-2 text-white">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-            <path fillRule="evenodd" d="M12.9 14.32a8 8 0 111.42-1.42l4.95 4.95-1.42 1.42-4.95-4.95zM8 14A6 6 0 108 2a6 6 0 000 12z" clipRule="evenodd" />
-          </svg>
-        </button>
       </div><button className="absolute top-6 right-6 text-white">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
