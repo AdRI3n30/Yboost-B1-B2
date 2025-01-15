75a4e128585aa406355bf132d5362a051c204401
Nounouxxx <marc.reimen@ynov.com>
2025-01-15 15:21:52 +0100

    update - BD

--- yboostb1-b2/src/yboostb2.sql
+++ yboostb1-b2/src/yboostb2.sql
@@ -21,6 +21,9 @@ SET time_zone = "+00:00";
 -- Base de données : `yboostb2`
 --
 
+CREATE DATABASE IF NOT EXISTS `yboostb2` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `yboostb2`;
+
 -- --------------------------------------------------------
 
 --
