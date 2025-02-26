8d0ab87dddd7dedabce46d847fcca0fe76b14d2b
TomBallauri <144777722+TomBallauri@users.noreply.github.com>
2025-02-26 08:59:53 +0100

    Menu Caroussel

--- yboostb1-b2/src/pages/List.tsx
+++ yboostb1-b2/src/pages/List.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import TaskList from '../Composant/tasks.tsx';
+import Caroussel from '../Composant/Caroussel.tsx';
+import Template_logo from '../Composant/template_logo.tsx';
+import NavigationBar from '../Composant/NavigationBar.tsx';
 
 const List = () => {
   return (
     <div >
-        <TaskList />
+        <Template_logo enableSearchBar={false} />
+        <Caroussel />
+        <NavigationBar />
     </div>
   );
 };
