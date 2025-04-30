5954cd1edf539d078dfbb4bbc512b0c1da58e6d1
AdRI3n30 <boreeadrien@gmail.com>
2025-04-30 09:50:04 +0200

    Update

---
+++ yboostb1-b2/src/Composant/NavigationBarWeb.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import LogoC from '../image/cocktails_icon.png';
+import back from '../public/Fond3.png';
+import { Link } from 'react-router-dom';
+
+interface NavigationBarWebProps {
+    enableButtonHome?: boolean;
+    enableButtonCocktails?: boolean;
+    enableButtonApropos?: boolean;
+    enableButtonPreparation?: boolean;
+}
+
+const NavigationBarWeb: React.FC<NavigationBarWebProps> = ({
+    enableButtonHome = false,
+    enableButtonCocktails = false,
+    enableButtonApropos = false,
+    enableButtonPreparation = false,
+}) => {
+    return (
+        <div className="absolute w-full h-20 p-2 flex bg-red-500 items-center justify-center" style={{ backgroundImage: `url(${back})` }}>
+            <div className="w-full h-full rounded-full border border-white bg-transparent flex items-center justify-between">
+                <div className="flex space-x-1 h-full">
+                    <Link to="/">
+                        <button
+                            className={`w-[160px] h-full rounded-full font-poppins border border-white bg-transparent text-white transition-all duration-300 ${
+                                enableButtonHome
+                                    ? "bg-white text-black w-[250px]"
+                                    : "hover:bg-white hover:text-black hover:w-[250px]"
+                            }`}
+                            style={
+                                enableButtonHome
+                                    ? { width: "180px", backgroundColor: "white", color: "black" }
+                                    : undefined
+                            }
+                        >
+                            Home
+                        </button>
+                    </Link>
+
+                    <Link to="/list">
+                        <button
+                            className={`w-[160px] h-full rounded-full border font-poppins border-white bg-transparent text-white transition-all duration-300 ${
+                                enableButtonCocktails
+                                    ? "bg-white text-black w-[250px]"
+                                    : "hover:bg-white hover:text-black hover:w-[250px]"
+                            }`}
+                            style={
+                                enableButtonCocktails
+                                    ? { width: "180px", backgroundColor: "white", color: "black" }
+                                    : undefined
+                            }
+                        >
+                            Cocktails
+                        </button>
+                    </Link>
+                </div>
+                <div className="flex space-x-1 h-full">
+                    <Link to="/apropos">
+                        <button
+                            className={`w-[160px] h-full rounded-full border font-poppins border-white ml-5 bg-transparent text-white transition-all duration-300 ${
+                                enableButtonApropos
+                                    ? "bg-white text-black w-[250px]"
+                                    : "hover:bg-white hover:text-black hover:w-[250px]"
+                            }`}
+                            style={
+                                enableButtonApropos
+                                    ? { width: "180px", backgroundColor: "white", color: "black" }
+                                    : undefined
+                            }
+                        >
+                            A propos
+                        </button>
+                    </Link>
+
+                    <Link to="/preparation">
+                        <button
+                            className={`w-[65px] h-full rounded-full border border-white bg-transparent text-white flex items-center justify-center group transition-all duration-300 ${
+                                enableButtonPreparation
+                                    ? "bg-white text-black w-[65px]"
+                                    : "hover:bg-white hover:text-black"
+                            }`}
+                            style={
+                                enableButtonPreparation
+                                    ? { backgroundColor: "white", color: "black" }
+                                    : undefined
+                            }
+                        >
+                            <img
+                                src={LogoC}
+                                alt="Cocktails Icon"
+                                className={`w-6 ml-[2px] h-6 transition-all duration-300 ${
+                                    enableButtonPreparation ? "invert" : "group-hover:invert"
+                                }`}
+                            />
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NavigationBarWeb;
--- yboostb1-b2/src/pages/Home.tsx
+++ yboostb1-b2/src/pages/Home.tsx
@@ -1,87 +1,89 @@
-import React, { useState, useEffect } from "react";
-import Landing from "../Composant/Landing.tsx"; 
-import { Link } from "react-router-dom";
-import CustomButton from "../Composant/Button.tsx";
-import Template_logo from "../Composant/template_logo.tsx";
-import SearchBar from "../Composant/SearchBar.tsx";
-import FooterButtons from "../Composant/FooterButton.tsx"; 
+import React from "react";
+import FondH from "../public/Fond3.png";
+import fleche from "../public/weui_arrow-outlined.png";
+import cock from "../public/cock.jpeg";
+import bg from "../public/bg.png";
+import NavigationBarWeb from "../Composant/NavigationBarWeb.tsx";
 
 const Home = () => {
-  const [isPortrait, setIsPortrait] = useState(window.innerHeight > window.innerWidth);
-  const [cocktails, setCocktails] = useState([]); 
-  const [searchQuery, setSearchQuery] = useState(""); 
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsPortrait(window.innerHeight > window.innerWidth);
-    };
-
-    const fetchCocktails = async () => {
-      try {
-        const response = await fetch('http://localhost:5000/cocktails');
-        const data = await response.json();
-        setCocktails(data); 
-      } catch (error) {
-        console.error("Erreur lors de la récupération des cocktails:", error);
-      }
-    };
-
-    window.addEventListener("resize", handleResize);
-    fetchCocktails(); 
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-
-  const filteredCocktails = cocktails.filter((cocktail) => {
-    const nameMatch = cocktail.Name.toLowerCase().includes(searchQuery.toLowerCase());
-    const ingredientsMatch = cocktail.Ingredients && Array.isArray(cocktail.Ingredients) &&
-      cocktail.Ingredients.some((ingredient) =>
-        ingredient.toLowerCase().includes(searchQuery.toLowerCase())
-      );
-
-    return nameMatch || ingredientsMatch; 
-  });
-
-  if (!isPortrait) {
-    return (
-      <div className="flex items-center justify-center h-screen bg-red-100 text-red-600">
-        <h1 className="text-3xl font-bold text-center">
-          Veuillez passer en mode portrait pour voir le contenu.
-        </h1>
-      </div>
-    );
-  }
-
   return (
-    <div className="bg-black h-screen">
-      <Template_logo />
-
-      <div className="flex justify-center mt-10">
-        <SearchBar setSearchQuery={setSearchQuery} /> 
-      </div>
-
-      <div className="absolute w-full h-auto bg-black mt-24 pb-20">
-        <div className="flex justify-center">
-          <div
-            id="contenue"
-            className="relative flex flex-wrap gap-8 justify-evenly mt-8 max-w-3xl"
-          >
-            {filteredCocktails.map((cocktail, index) => (
-              <Landing 
-                key={index} 
-                name={cocktail.Name} 
-                image={cocktail.Image}
-              />
-            ))}
+    <>
+      <section className=" p-4 relative flex justify-center w-full h-screen mb-12">
+      
+        <div className="w-full bg-cover bg-center relative rounded-[25px] shadow-lg " style={{ backgroundImage: `url(${FondH})` }}>
+        <NavigationBarWeb enableButtonHome={true} />
+          <div className="w-[25px] h-[25px] absolute bg-red-0 bottom-0 right-[305px] rounded-full shadow-[8.5px_9px_0_#fff]">
           </div>
+          <div className="w-[25px] h-[25px] absolute bg-red-0 bottom-[73px] right-[0px] rounded-full shadow-[8.5px_9px_0_#fff]">
+          </div>
+          <div className="absolute top-[35%] left-8 text-left text-white">
+              <h1 className="text-2xl sm:text-4xl md:text-6xl font-poppins  mb-4">ShakeLabs</h1>
+              <p className="text-sm sm:text-lg md:text-xl font-poppins font-light max-w-xl">
+                Bienvenue dans l’univers des cocktails où chaque recette <br />
+                raconte une histoire et chaque gorgée éveille les sens
+              </p>
+              </div>
         </div>
-      </div>
 
-      <div className="bg-black h-screen">
-        <FooterButtons /> 
-      </div>
-    </div>
+          <div className=" absolute bg-white w-[325px] h-[90px] bottom-0 right-0 border-white border-8 rounded-3xl ">
+          </div>
+
+          <button
+              onClick={() => (window.location.href = "/recipes")}
+              className=" absolute bottom-[2%] right-[1.5%] bg-white text-black w-[290px] h-[60px] rounded-[200px] shadow-md hover:bg-gray-200 transition duration-300 border-2 border-black text-sm sm:text-xl"
+            >
+              Explorer les recettes
+            </button>
+
+            <button
+              onClick={() => (window.location.href = "#A_propos")}
+              className=" absolute bg-center bg-contain w-[64px] h-[64px] bottom-[10%] left-[5%] rounded-[50%] border border-white border-2 "
+            >
+              <img src={fleche} alt="fleche" />
+            </button>
+      </section> 
+
+        <section className="mb-20 w-full"  style={{ backgroundImage: `url(${bg})`, backgroundPosition: 'center', backgroundSize: 'cover' }}>
+          <div className="p-12 text-center ">
+            <h1 className="text-4xl  font-poppins text-black">
+              Plus qu’un cocktail, une <strong>expérience</strong>
+            </h1>
+            <p className="text-2xl text-black mt-5 px-4  font-poppins mx-auto">
+              Chez ShakeLabs, chaque boisson est pensée comme un voyage sensoriel.
+              <br />
+              Des classiques revisités aux créations originales, nous explorons
+              l’art du cocktail avec passion, précision… et <br />
+              une pointe d’audace.
+            </p>
+          </div>
+
+          <div className="mt-20 w-full flex items-around ">
+            <div className="pl-[15%] flex justify-center items-center flex-col w-full">
+              <h1 className=" text-4xl font-poppins font-semibold mb-6">
+                Notre Mission ?
+              </h1>
+              <p className="text-center text-2xl">
+                Faire découvrir (ou redécouvrir) le plaisir de savourer <br />
+                un bon cocktail, que ce soit chez soi ou entre amis, en <br />
+                proposant des recettes accessibles, des conseils de pro <br />
+                et des inspirations venues du monde entier.
+              </p>
+              
+            </div>
+            <div className="w-full flex justify-center items-center">
+              <div className="bg-gray-500 w-[50%] h-[250px] sm:h-[500px] rounded-2xl shadow-xl hover:scale-105 transition-transform duration-300" style={{ backgroundImage: `url(${cock})`, backgroundPosition: 'center', backgroundSize: 'cover' }} />
+            </div>
+          </div>
+
+          <div className="mt-20 text-center px-4">
+            <h1 className="text-3xl font-poppins text-black">
+              <strong>Découvrez</strong> des cocktails et leurs recettes incontournables <br />
+              pour <strong>épater</strong> vos invités et <strong>éveiller</strong> vos papilles
+            </h1>
+            <div className="w-1/2 sm:w-1/3 h-[2px] bg-black mt-4 mx-auto"></div>
+          </div>
+        </section>
+    </>    
   );
 };
 
